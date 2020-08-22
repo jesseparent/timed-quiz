255f30b6c1969b10e805cfbd65065260076caa84
Jesse Parent <jesseparent@gmail.com>
2020-08-21 23:14:55 -0600

    Added quiz template

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,16 @@
 let countdownTimer = document.querySelector("#countdown");
-let goBtn = document.querySelector("#btnStart");
+let pageContent = document.querySelector("#page-content");
+let quizContent = document.querySelector("#quiz-content-template");
+
+let goBtn;
+let aBtn;
+let bBtn;
+let cBtn;
+let dBtn;
+
+let startHtml = pageContent.innerHTML;
+let quizHtml = quizContent.innerHTML;
+
 var startTime = 5;
 var thisInterval;
 
@@ -12,6 +23,44 @@ let startTimer = function () {
       clearInterval(thisInterval);
     }
   }, 1000);
+};
+
+let startQuiz = function() {
+  pageContent.innerHTML = quizHtml;
+  addAnswerListeners();
 }
 
-goBtn.addEventListener("click", startTimer);
+let checkAnswer = function(event) {
+  console.log(event.target.value);
+};
+
+let addStartListeners = function() {
+  goBtn = document.querySelector("#btnStart");
+
+  goBtn.addEventListener("click", main);
+}
+
+let addAnswerListeners = function() {
+  aBtn = document.querySelector("#btnA");
+  bBtn = document.querySelector("#btnB");
+  cBtn = document.querySelector("#btnC");
+  dBtn = document.querySelector("#btnD");
+
+  aBtn.addEventListener("click", checkAnswer);
+  bBtn.addEventListener("click", checkAnswer);
+  cBtn.addEventListener("click", checkAnswer);
+  dBtn.addEventListener("click", checkAnswer);
+};
+
+let destroyTemplates = function() {
+  quizContent.innerHTML = "";
+}
+
+let main = function() {
+  startTimer();
+  startQuiz();
+};
+
+addStartListeners();
+
+//destroyTemplates();
