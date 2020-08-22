d611bbd9e3ad1bb2fea1167a24493d86bec8281a
Jesse Parent <jesseparent@gmail.com>
2020-08-22 01:05:18 -0600

    Start Quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,30 @@
-let countdownTimer = document.querySelector("#countdown");
-let pageContent = document.querySelector("#page-content");
-let quizContent = document.querySelector("#quiz-content-template");
+var questions = [
+  {
+    "question": "Question 1",
+    "answer1": "Answer 1",
+    "answer2": "Answer 2",
+    "answer3": "Answer 3",
+    "answer4": "Answer 4",
+    "correctAnswer": "1"
+  },
+  {
+    "question": "Question 2",
+    "answer1": "Answer 1",
+    "answer2": "Answer 2",
+    "answer3": "Answer 3",
+    "answer4": "Answer 4",
+    "correctAnswer": "1"
+  }
+]
+console.dir(questions);
+
+let headerEl = document.querySelector("#header");
+let countdownTimerEl = document.querySelector("#countdown");
+let pageContentEl = document.querySelector("#page-content");
+let resultsWrapperEl;
+let quizContentEl = document.querySelector("#quiz-content-template");
+let resultsContentEl = document.querySelector("#results-content-template");
+let highScoresContentEl = document.querySelector("#highscores-content-template");
 
 let goBtn;
 let aBtn;
@@ -8,39 +32,57 @@ let bBtn;
 let cBtn;
 let dBtn;
 
-let startHtml = pageContent.innerHTML;
-let quizHtml = quizContent.innerHTML;
+let startHtml = pageContentEl.innerHTML;
+let quizHtml = quizContentEl.innerHTML;
+let resultsHtml = resultsContentEl.innerHTML;
+let higScoresHtml = highScoresContentEl.innerHTML;
 
-var startTime = 5;
+var startTime = 75;
 var thisInterval;
 
 let startTimer = function () {
-  countdownTimer.textContent = startTime;
+  countdownTimerEl.textContent = startTime;
   clearInterval(thisInterval);
   thisInterval = setInterval(function () {
-    countdownTimer.textContent = --startTime;
+    countdownTimerEl.textContent = --startTime;
     if (startTime === 0) {
       clearInterval(thisInterval);
     }
   }, 1000);
 };
 
-let startQuiz = function() {
-  pageContent.innerHTML = quizHtml;
+let startQuiz = function () {
+  pageContentEl.innerHTML = quizHtml;
   initAnswerListeners();
-}
 
-let checkAnswer = function(event) {
-  console.log(event.target.value);
+  displayQuestion(0);
 };
 
-let initStartListeners = function() {
+let displayQuestion = function (questionNum) {
+  pageContentEl.querySelector("#question").textContent = questions[questionNum].question;
+  pageContentEl.querySelector("#questionIndex").value = questionNum;
+  for (let i = 1; i <= 4; i++) {
+    eval('pageContentEl.querySelector("#btnAnswer' + i + '").textContent = "' + i + '. " + questions[questionNum].answer' + i + ';');
+  }
+};
+
+let checkAnswer = function (event) {
+  let questionIndex = pageContentEl.querySelector("#questionIndex").value;
+  let userAnswer = event.target.value;
+
+  console.log(userAnswer === questions[questionIndex].correctAnswer);
+  questionIndex++;
+  console.log("next index: "+ questionIndex);
+  displayQuestion(questionIndex);
+};
+
+let initStartListeners = function () {
   goBtn = document.querySelector("#btnStart");
 
   goBtn.addEventListener("click", main);
 }
 
-let initAnswerListeners = function() {
+let initAnswerListeners = function () {
   answer1Btn = document.querySelector("#btnAnswer1");
   answer2Btn = document.querySelector("#btnAnswer2");
   answer3Btn = document.querySelector("#btnAnswer3");
@@ -52,15 +94,18 @@ let initAnswerListeners = function() {
   answer4Btn.addEventListener("click", checkAnswer);
 };
 
-let destroyTemplates = function() {
-  quizContent.innerHTML = "";
-}
+// Just in case there are conflicts with ids in templates, get rid of them
+let destroyTemplates = function () {
+  quizContentEl.innerHTML = "";
+  resultsContentEl.innerHTML = "";
+  highScoresContentEl.innerHTML = "";
+};
 
-let main = function() {
+let main = function () {
   startTimer();
   startQuiz();
 };
 
 initStartListeners();
 
-//destroyTemplates();
+destroyTemplates();
